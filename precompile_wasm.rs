9e9462ed6c2b66d2552bce3cd76d38099de4d4ec
Antoine Lavandier <antoine.lavandier@inria.fr>
2025-10-22 09:45:39 +0200

    Script to compile crates into wasmtime's 'precompiled' format
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

---
+++ precompile_wasm.rs
@@ -0,0 +1,235 @@
+#!/usr/bin/env -S cargo +nightly -Zscript
+---cargo
+[package]
+edition = "2024"
+
+[dependencies]
+clap = { version = "4.5.40", features = ["derive"]}
+wasmtime = { git = "https://github.com/bytecodealliance/wasmtime", rev = "a70809d0caa4959b5d991c2770de821ab3c63930", default-features = false, features = ["component-model", "async", "cranelift", "pulley"] }
+miette = { version = "7.2.0", features = ["fancy"] }
+thiserror = { version = "2.0.12" }
+
+---
+#![feature(trim_prefix_suffix)]
+use std::{fs, io, path::{PathBuf, Path}};
+use std::io::BufRead as _;
+use clap::Parser;
+use miette::Diagnostic;
+
+use wasmtime::{Config, Engine};
+
+
+/// Simple CLI that takes a compiled .wasm file and turns into a precompiled-component
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the compiled Wasm file or to a manifest for a lib that will be compiled
+    #[arg(short, long)]
+    path: PathBuf,
+
+    /// Path to an optional `config.toml` file when `path` points to a manifest
+    #[arg(long)]
+    config: Option<PathBuf>,
+
+    /// Toolchain for the compilation when `path` points to a manifest
+    #[arg(short, long)]
+    toolchain: Option<String>,
+
+    /// Additionnal compilation options propagated to cargo
+    #[arg(long = "compile_options", short='C')]
+    additional: Vec<String>,
+
+    /// Turn fuel instrumentation on
+    #[arg(short, long)]
+    fuel: bool,
+
+    /// Path of the output file
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+
+    /// Path to wasm-tools if isn't not in $PATH
+    #[arg(short, long)]
+    wasm_tools: Option<PathBuf>,
+
+    /// Outputs a Module instead of a component
+    #[arg(short, long)]
+    module: bool,
+}
+
+#[derive(Debug, thiserror::Error, Diagnostic)]
+enum Error {
+    #[error("I/O error: {0}")]
+    Io(#[from] io::Error),
+
+    #[error("Precompilation Error: {0}")]
+    Precomp(#[from] wasmtime::Error),
+}
+
+
+fn main() -> miette::Result<()> {
+    let args = Args::parse();
+
+    let Args { path, config, toolchain, additional, fuel, output, wasm_tools, module } = args;
+
+    // Check that the path exists
+    assert!(fs::exists(&path).map_err(Error::from)?);
+
+    let new_path = match path.extension().map(std::ffi::OsStr::to_str).flatten() {
+        // If it's a already a compiled wasm do nothing
+        Some("wasm") => {
+            path
+        },
+        // Means that it's a directory
+        Some("toml") => {
+            std::println!("Compiling Library");
+            let manifest = io::BufReader::new(fs::File::open(&path).map_err(Error::from)?);
+            let name = manifest.lines().into_iter().filter_map(
+                |line| {
+                    match line {
+                        Ok(l) => {
+                            if l.starts_with("name =") {
+                                Some(l.split("=").nth(1).unwrap().split("\"").nth(1).unwrap().to_owned().replace('-', "_"))
+                            }
+                            else {
+                                None
+                            }
+                        }
+                        Err(_) => None
+                    }
+                }
+            ).next().unwrap();
+
+            let mut args: Vec<&str> =  match toolchain  {
+                Some(toolchain) => {
+                    if toolchain.starts_with("+") {
+                        vec![toolchain.leak()]
+                    }
+                    else {
+                        vec!["+", toolchain.leak()]
+                    }
+                }
+                None => {
+                    vec![]
+                }
+            };
+
+            args.extend(["build", "--release", "--manifest-path", path.as_path().to_str().unwrap()]);
+
+            if let Some(config_path) = config {
+                    match config_path.extension().map(std::ffi::OsStr::to_str).flatten() {
+                    Some("toml") => {
+                        args.push("--config");
+                        args.push(config_path.to_string_lossy().into_owned().leak());
+                    }
+                    _ => Err(io::Error::new(io::ErrorKind::InvalidInput, "--config should point to a cargo config file")).map_err(Error::from)?,
+                }
+            }
+
+
+            args.extend(additional.iter().map(String::as_str));
+
+
+            args.extend(["--target-dir", "temp"]);
+
+            std::println!("{:?}", args);
+
+            let output = std::process::Command::new("cargo")
+                .args(&args)
+                .output()
+                .map_err(Error::from)?;
+
+            let std::process::Output {status, stdout: _, stderr} = output;
+
+            if !status.success() {
+                std::println!(
+                    "{}", String::from_utf8_lossy(&stderr)
+                );
+
+                return Err(io::Error::new(io::ErrorKind::InvalidInput, "Module compilation failed")).map_err(Error::from)?;
+            }
+
+
+            format!("temp/wasm32v1-none/release/{name}.wasm").into()
+        },
+        _ => {
+            Err(io::Error::new(io::ErrorKind::InvalidInput, "--path should be wasm file or a path to Cargo.toml manifest")).map_err(Error::from)?
+        }
+    };
+
+    std::println!("{}", new_path.display());
+
+    if !module {
+        // Turn into a component
+        std::println!("Turning the Module into a component");
+        let wasm_tools_path = if wasm_tools.is_some() {
+            wasm_tools.unwrap()
+        } else {
+            "wasm-tools".into()
+        };
+        // Note: On Unix, non UTF8 strings are invalid Paths so the unwrap() is infallible
+        let output = std::process::Command::new(wasm_tools_path)
+            .args(["component", "new", new_path.as_path().to_str().unwrap(), "-o", "temp.wasm"])
+            .output()
+            .map_err(Error::from)?;
+
+        let std::process::Output {status, stdout: _, stderr} = output;
+
+        if !status.success() {
+            std::println!(
+                "{}", String::from_utf8_lossy(&stderr)
+            );
+            return Err(io::Error::new(io::ErrorKind::InvalidInput, "Componentization failed")).map_err(Error::from)?;
+        }
+    }
+
+    let out = if output.is_some() {
+        output.unwrap()
+    } else {
+        String::from("payload.cwasm").into()
+    };
+    if !module {
+        precompile("temp.wasm", fuel, out, module)?;
+        std::fs::remove_file("temp.wasm").map_err(Error::from)?;
+    } else {
+        precompile(&new_path, fuel, out, module)?;
+    }
+    if fs::exists("temp").map_err(Error::from)? {
+        std::fs::remove_dir_all("temp").map_err(Error::from)?;
+    }
+
+    Ok(())
+}
+
+fn precompile<P: AsRef<Path>>(path: P, fuel: bool, out: PathBuf, module: bool) -> miette::Result<()> {
+    std::println!("Precompiling Wasm Module/Component");
+    let mut config = Config::new();
+    // Tell how the memory is expected to behave when instantiated
+    config.memory_reservation(0);
+    config.wasm_custom_page_sizes(true);
+    config.memory_may_move(false);
+    config.memory_init_cow(false);
+
+
+    // Optimize for Code size in the .cwasm
+    config.cranelift_opt_level(wasmtime::OptLevel::SpeedAndSize);
+    config.target("pulley32").map_err(Error::from)?;
+
+    // Enable fuel intstrumentation to prevent malevolent code from running indefinitely in the VM
+    config.consume_fuel(fuel);
+
+    // Create an `Engine` with that configuration.
+    let engine = Engine::new(&config).map_err(Error::from)?;
+
+    std::println!("Reading Module/Component File");
+    let wasm = fs::read(path).map_err(Error::from)?;
+    let precompiled = if !module {
+        engine.precompile_component(&wasm).map_err(Error::from)?
+    } else {
+        engine.precompile_module(&wasm).map_err(Error::from)?
+    };
+
+    std::println!("Writing the precompiled file");
+    fs::write(out, &precompiled).map_err(Error::from)?;
+
+    Ok(())
+}
