3237cecc8cccbdf3f5fdd52abf702e864f9161ea
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-09-03 14:51:13 +0200

    feat: add sandbox-no-bindings to the rebuild file
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- rebuild-all-payloads.sh
+++ rebuild-all-payloads.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+# set -e
 
 
 for p in ble-scanner ephemeral-no-bindings ephemeral-with-bindings gpio persistent-no-bindings persistent-with-bindings udp-bindings
@@ -20,3 +20,13 @@ cp examples/async-bindings/payload.cwasm examples/updatable-async/async-payload.
 cp examples/async-bindings/payload.cwasm examples/suit-updatable/payload.cwasm
 
 cargo +nightly-2026-04-28 -Z script precompile_wasm.rs --path payloads/sensors/Cargo.toml -o examples/fake-sensor/payload.cwasm --config payloads/.cargo/config.toml --toolchain +nightly-2026-04-28
+
+cd payloads/sandbox-no-bindings
+FIB_NUM=10 cargo +nightly-2026-04-28 build --release --config ../.cargo/config.toml
+mv target/wasm32v1-none/release/sandbox_no_bindings.wasm fib-10.wasm
+FIB_NUM=30 cargo +nightly-2026-04-28 build --release --config ../.cargo/config.toml
+mv target/wasm32v1-none/release/sandbox_no_bindings.wasm fib-30.wasm
+cd ../..
+cargo +nightly-2026-04-28 -Z script precompile_wasm.rs --path payloads/sandbox-no-bindings/fib-10.wasm -o examples/sandbox-no-bindings/fib-10.cwasm
+cargo +nightly-2026-04-28 -Z script precompile_wasm.rs --path payloads/sandbox-no-bindings/fib-30.wasm -o examples/sandbox-no-bindings/fib-30.cwasm
+rm payloads/sandbox-no-bindings/fib-10.wasm payloads/sandbox-no-bindings/fib-30.wasm
